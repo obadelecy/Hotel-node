f7a3502cf2812415cdd1dff9fbc94032aed9dccf
obadelecy <obadelecy@outlook.com>
2025-04-07 21:01:34 -0300

    Adição do arquivo autenticador_middleware

--- app/routes/router.js
+++ app/routes/router.js
@@ -3,24 +3,36 @@ const usuarioModel = require("../models/usuarioModel");
 const router = express.Router();
 const {body, validationResult} = require('express-validator')
 
+const { verificarUsuAutenticado, limparSessao, gravarUsuAutenticado, verificarUsuAutorizado } = require("../models/autenticador_middleware");
+
 const usuarioController = require("../controllers/usuarioController")
 
 
-router.get("/", (req, res)=>{
-    res.render("pages/index");
+router.get("/", verificarUsuAutenticado, function (req, res) {
+    res.render("pages/index", req.session.autenticado);
 });
 
-router.get("/login", (req, res)=>{
-    res.render("pages/login");
-} )
+router.get("/login", function (req, res) {
+    res.render("pages/login", { listaErros: null });
+});
 
-router.get("/cadastro", (req, res)=>{
-    res.render("pages/cadastro", {"listaErros":null, "valores":{nome:"", senha:"", "csenha":"", email:""}, "falha":null})
-})
+router.post(
+    "/login",
+    usuarioController.regrasValidacaoFormLogin,
+    gravarUsuAutenticado,
+    function (req, res) {
+      usuarioController.logar(req, res);
+});
 
-router.post("/cadastro", usuarioController.validacaoFormularioCadastro, (req, res) => {
-    usuarioController.inserirUsuario(req, res)
-})
+router.get("/cadastro", function (req, res) {
+    res.render("pages/cadastro", { listaErros: null, valores: { nome_usu: "", nomeusu_usu: "", email_usu: "", senha_usu: "" } });
+});
+
+router.post("/cadastro",
+    usuarioController.regrasValidacaoFormCad,
+    async function (req, res) {
+        usuarioController.cadastrar(req, res);
+});
 
 router.get("/perfil", (req, res)=>{
     res.render("pages/perfil")
